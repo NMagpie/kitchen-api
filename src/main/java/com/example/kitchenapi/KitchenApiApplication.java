32f65b35a29f2fca17666680f3dc3a4842d28e5a
NMagpie <sorochin.work@gmail.com>
2021-09-27 02:15:29 +0300

    Initialization of the project

---
+++ src/main/java/com/example/kitchenapi/KitchenApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitchenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenApiApplication.class, args);
+	}
+
+}
